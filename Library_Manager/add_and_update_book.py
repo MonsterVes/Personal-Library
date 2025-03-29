ed56b9c7b104c585ad07b7756a3d2d59cf4fad92
MonsterVes <vszaprianova@gmail.com>
2025-03-30 01:17:57 +0200

    first modules

---
+++ Library_Manager/add_and_update_book.py
@@ -0,0 +1,65 @@
+from book import Book
+from serialization_functions import add_to_json_book_file
+import pyinputplus as pyip
+
+
+def add_book():
+
+    """
+    Gather book info input from the user in order to create a new Book object and add it to the Book_library.json file
+    """
+    while True:
+
+        try:
+
+            # Gather input
+
+            title = input("Enter book title: ").title()
+            author = input("Enter author name: ").title()
+            isbn = pyip.inputNum("Enter ISBN: ")
+            year_obj = pyip.inputDate("Enter publishing year (YYYY-MM-DD): ", formats = ["%Y-%m-%d"])
+            year = year_obj.strftime("%Y-%m-%d")
+            publisher = input("Enter publisher name: ").capitalize()
+            print("Enter book genre: ")
+            genre = pyip.inputMenu(["Action", "Adventure", "Children", "Comedy", "Crime", "Drama", "Fantasy", 
+                                    "Historical", "Mystery", "Poetry", "Science", "Sci-Fi"], numbered = True).capitalize()
+            pages = pyip.inputInt("Enter number of pages: ", min=3)
+            format = pyip.inputChoice(["Paperback", "Hardcover", "E-book", "Audiobook"], 
+                                      prompt = "Enter book format (Paperback, Hardcover, E-book, Audiobook): ").capitalize()
+            price = pyip.inputFloat("Enter book price: ", min = 0.1)
+            purchase_date_obj = pyip.inputDate("Enter date of purchase (YYYY-MM-DD): ", formats = ["%Y-%m-%d"])
+            
+            # Validate purchase date is after the publishing date
+
+            if purchase_date_obj > year_obj:
+                purchase_date = purchase_date_obj.strftime("%Y-%m-%d")
+            else:
+
+                # Start from the begining in case the publishing year has been inputet with an error.
+
+                print("Invalid purchase date. Purchase date is prior to Publishing year. Please re-enter book info.")
+                continue
+
+            personal_rating = pyip.inputChoice(["1","2","3","4","5"], prompt = "Enter personal rating (1-5): ")
+            status = pyip.inputChoice(["Read", "Want to read", "In progress", "Unread"], 
+                                      prompt = "Enter read status (Read, Want to read, In progress, Unread): ").capitalize()
+            notes = input("Enter any additional notes: ")
+
+            # Initialize new book object 
+
+            new_book = Book(title, author, isbn, year, publisher, genre, pages, format, price, 
+                    purchase_date, personal_rating, status, notes)
+            
+            # Add the new book to the Book_library.json file
+
+            add_to_json_book_file(new_book)
+            break
+
+        except ValueError as e:
+            print(f"Invalid input: {e}. Please try again.")
+        except Exception as e:
+            print(f"An error occurred: {e}. Please try again.")
+
+
+
+
---
+++ Library_Manager/book.py
@@ -0,0 +1,102 @@
+
+
+
+class Book:
+    def __init__(self, title, author, isbn, year, publisher, genre, pages, format, price, 
+                 purchase_date, personal_rating, status, notes):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.year = year
+        self.publisher = publisher
+        self.genre = genre
+        self.pages = pages
+        self._format = format
+        self.price = price
+        self.purchase_date = purchase_date
+        self._personal_rating = personal_rating
+        self._status = status
+        self.notes = notes
+
+
+    def __str__(self):
+        return (
+            f"Title: {self.title}\n"
+            f"Author: {self.author}\n"
+            f"ISBN: {self.isbn}\n"
+            f"Publishing year: {self.year}\n"
+            f"Publisher: {self.publisher}\n"
+            f"Genre: {self.genre}\n"
+            f"Pages: {self.pages}\n" 
+            f"Format: {self.format}\n"
+            f"Price EUR: {self.price}\n" 
+            f"Purchase date: {self.purchase_date}\n"
+            f"Personal rating: {self.personal_rating}\n"
+            f"Status: {self.status}\n"
+            f"Notes: {self.notes}\n"
+        )
+    
+    
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "author": self.author,
+            "isbn": self.isbn,
+            "year": self.year,
+            "publisher": self.publisher,
+            "genre": self.genre,
+            "pages": self.pages, 
+            "format": self.format,
+            "price": self.price, 
+            "purchase_date": self.purchase_date,
+            "personal_rating": self.personal_rating,
+            "status": self.status,
+            "notes": self.notes 
+        }
+    
+
+    """Checks for allowed Personal ratings"""
+    @property
+    def personal_rating(self):
+        return self._personal_rating
+    
+    @personal_rating.setter
+    def personal_rating(self, personal_rating):
+        if personal_rating not in ["1", "2", "3", "4", "5"]:
+            raise ValueError("Invalid rating. Allowed ratings: 1, 2, 3, 4, 5")
+        self._personal_rating = personal_rating
+
+
+    """Check for allowed book formats"""
+    @property
+    def format(self):
+        return self._format
+    
+    @format.setter
+    def format(self, format):
+        if format not in ["Paperback", "Hardcover", "E-book", "Audiobook"]:
+            raise ValueError("Invalid Format. Allowed formats: Paperback, Hardcover, E-book, Audiobook")
+        self._format = format
+
+
+    """Check for allowed Personal read statuses"""
+    @property
+    def status(self):
+        return self._status
+    
+    @status.setter
+    def status (self, status):
+        if status not in ["Read", "Want to read", "In progress", "Unread"]:
+            raise ValueError ("Invalid read status. Allowed statuses: Read, Want to read, In progress, Unread")
+        self._status = status
+
+
+
+
+
+
+# book1 = Book("Dune", "Frank Herbert", "0441172717", "1990-09-01", "Ace", "Sci-fi", "896", "Paperback", "7.52", 
+#                  "2001-12-10", "5", "Read", "Great Sci-Fi book")
+# book2 = Book("Dune the messiah", "Frank Herbert", "0441172718", "1995-09-01", "Ace", "Sci-fi", "263", "Paperback", "5.42", 
+#                  "2001-12-10", "4", "Read", "Sequel to Dune. Not horrible")
+
