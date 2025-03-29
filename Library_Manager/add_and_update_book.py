17a711d6a506ef1e09c0abdebaae67cd1650971c
MonsterVes <vszaprianova@gmail.com>
2025-03-30 01:44:11 +0200

    save conflict

--- Library_Manager/add_and_update_book.py
+++ Library_Manager/add_and_update_book.py
@@ -4,92 +4,57 @@ import pyinputplus as pyip
 
 
 def add_book():
+
     """
     Gather book info input from the user in order to create a new Book object and add it to the Book_library.json file
     """
     while True:
+
         try:
+
             # Gather input
 
             title = input("Enter book title: ").title()
             author = input("Enter author name: ").title()
             isbn = pyip.inputNum("Enter ISBN: ")
-            year_obj = pyip.inputDate(
-                "Enter publishing year (YYYY-MM-DD): ", formats=["%Y-%m-%d"]
-            )
+            year_obj = pyip.inputDate("Enter publishing year (YYYY-MM-DD): ", formats = ["%Y-%m-%d"])
             year = year_obj.strftime("%Y-%m-%d")
             publisher = input("Enter publisher name: ").capitalize()
 
             # Select a genre from a menu list
-
             print("Enter book genre: ")
-            genre = pyip.inputMenu(
-                [
-                    "Action",
-                    "Adventure",
-                    "Children",
-                    "Comedy",
-                    "Crime",
-                    "Drama",
-                    "Fantasy",
-                    "Historical",
-                    "Mystery",
-                    "Poetry",
-                    "Science",
-                    "Sci-Fi",
-                ],
-                numbered=True,
-            ).capitalize()
-
+            genre = pyip.inputMenu(["Action", "Adventure", "Children", "Comedy", "Crime", "Drama", "Fantasy", 
+                                    "Historical", "Mystery", "Poetry", "Science", "Sci-Fi"], numbered = True).capitalize()
+            
             pages = pyip.inputInt("Enter number of pages: ", min=3)
-            format = pyip.inputChoice(
-                ["Paperback", "Hardcover", "E-book", "Audiobook"],
-                prompt="Enter book format (Paperback, Hardcover, E-book, Audiobook): ",
-            ).capitalize()
-            price = pyip.inputFloat("Enter book price: ", min=0.1)
-            purchase_date_obj = pyip.inputDate(
-                "Enter date of purchase (YYYY-MM-DD): ", formats=["%Y-%m-%d"]
-            )
 
+            format = pyip.inputChoice(["Paperback", "Hardcover", "E-book", "Audiobook"], 
+                                      prompt = "Enter book format (Paperback, Hardcover, E-book, Audiobook): ").capitalize()
+            
+            price = pyip.inputFloat("Enter book price: ", min = 0.1)
+            
+            
             # Validate purchase date is after the publishing date
-
+            purchase_date_obj = pyip.inputDate("Enter date of purchase (YYYY-MM-DD): ", formats = ["%Y-%m-%d"])
             if purchase_date_obj > year_obj:
                 purchase_date = purchase_date_obj.strftime("%Y-%m-%d")
             else:
+
                 # Start from the begining in case the publishing year has been inputet with an error.
 
-                print(
-                    "Invalid purchase date. Purchase date is prior to Publishing year. Please re-enter book info."
-                )
+                print("Invalid purchase date. Purchase date is prior to Publishing year. Please re-enter book info.")
                 continue
 
-            personal_rating = pyip.inputChoice(
-                ["1", "2", "3", "4", "5"], prompt="Enter personal rating (1-5): "
-            )
-            status = pyip.inputChoice(
-                ["Read", "Want to read", "In progress", "Unread"],
-                prompt="Enter read status (Read, Want to read, In progress, Unread): ",
-            ).capitalize()
+            personal_rating = pyip.inputChoice(["1","2","3","4","5"], prompt = "Enter personal rating (1-5): ")
+            status = pyip.inputChoice(["Read", "Want to read", "In progress", "Unread"], 
+                                      prompt = "Enter read status (Read, Want to read, In progress, Unread): ").capitalize()
             notes = input("Enter any additional notes: ")
 
-            # Initialize new book object
+            # Initialize new book object 
 
-            new_book = Book(
-                title,
-                author,
-                isbn,
-                year,
-                publisher,
-                genre,
-                pages,
-                format,
-                price,
-                purchase_date,
-                personal_rating,
-                status,
-                notes,
-            )
-
+            new_book = Book(title, author, isbn, year, publisher, genre, pages, format, price, 
+                    purchase_date, personal_rating, status, notes)
+            
             # Add the new book to the Book_library.json file
 
             add_to_json_book_file(new_book)
@@ -99,3 +64,7 @@ def add_book():
             print(f"Invalid input: {e}. Please try again.")
         except Exception as e:
             print(f"An error occurred: {e}. Please try again.")
+
+
+
+
