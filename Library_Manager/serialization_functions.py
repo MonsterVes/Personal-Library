1a6b11485e0f113b5950ccd3e9e4b7b033a5a788
MonsterVes <vszaprianova@gmail.com>
2025-03-30 01:41:45 +0200

    ruff fix and format

--- Library_Manager/serialization_functions.py
+++ Library_Manager/serialization_functions.py
@@ -1,5 +1,6 @@
 import json
 
+
 def add_to_json_book_file(book, file_name="Book_library.json"):
     """
     Function that adds the book info to a JSON file where all the books will be stored
@@ -11,16 +12,15 @@ def add_to_json_book_file(book, file_name="Book_library.json"):
         book_data = {}
     except Exception:
         print("Ups.. Something went wrong")
-    
+
     book_data[book.title] = book.to_dict()
-    
+
     with open(file_name, "w") as file:
         json.dump(book_data, file, indent=4)
-    
-    
-    print("-~"*25)
+
+    print("-~" * 25)
     print(f"{book.title} has been added to the Library")
-    print("-~"*25)
+    print("-~" * 25)
+
 
 # def get_from_json_book_file(book, file_name="Book_library.json"):
-
